65b99d30b9114930c5cee92e400056886f221a6b
chrismuto <chrismuto@gmail.com>
2022-06-21 10:20:27 -0700

    added text animations and styling

---
+++ js/script.js
@@ -0,0 +1,67 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#sideHeader1", {
+    scrollTrigger: "#sideHeader1",
+    duration: 1.25,
+    x: -300, opacity: 0,
+    paused: true,
+    delay: 1
+});
+
+gsap.from("#sideHeader2", {
+    scrollTrigger: "#sideHeader2",
+    duration: 1.25,
+    x: -300, opacity: 0,
+    paused: true,
+    delay: 1
+});
+
+gsap.from("#sideHeader3", {
+    scrollTrigger: "#sideHeader3",
+    duration: 1.25,
+    x: -300, opacity: 0,
+    paused: true,
+    delay: 1
+});
+
+gsap.from("#scrollHeader1", {
+    scrollTrigger: "#scrollHeader1",
+    y: 50,
+    opacity: 0,
+    paused: true,
+    delay: 1
+})
+
+gsap.from("#scrollHeader2", {
+    scrollTrigger: "#scrollHeader2",
+    y: 50,
+    opacity: 0,
+    paused: true,
+    delay: 1})
+
+gsap.from("#scrollHeader3", {
+    scrollTrigger: "#scrollHeader3",
+    y: 50,
+    opacity: 0,
+    paused: true,
+    delay: 1})
+
+gsap.from("#scrollHeader4", {
+    scrollTrigger: "#scrollHeader4",
+    y: 50,
+    opacity: 0,
+    paused: true,
+    delay: 1})
+
+gsap.from("#scrollHeader5", {
+    scrollTrigger: "#scrollHeader5",
+    y: 50,
+    opacity: 0,
+    paused: true,
+    delay: 1})
+
+if (ScrollTrigger.isInViewport("#carousel-wrapper", 0)) {
+    gsap.from("#nav-wrapper", {
+    backgroundColor: f68846
+})
+}
